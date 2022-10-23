bf2e19f1a4b139cb9177563f717339aaedb8367e
mdOmarfaruk151 <mdomar28151@gmile.com>
2022-10-23 21:19:51 +0600

    solve img hosting problem

--- src/components/Run Time/Time.js
+++ src/components/Run Time/Time.js
@@ -1,24 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const Time = ({list}) => {
-     // oo
-  let total =0;
-  // const [minData, setMinData] =useState(0)
-  for(const min of list){
-    total =total + min[0].time;
+const Time = ({ list }) => {
+  // oo
+  let total = 0;
+  // const [minData, setMinData] =useState(0);
+  for (const min of list) {
+    total = total + min[0].time;
     // setMinData(total)
     // localStorage.setItem('min', total);
-  };
-  
+  }
+
   // useEffect(()=>{
   //   const data1 = localStorage.getItem('min');
   //  data1 && setMinData(data1);
   // },[]);
-    return (
-        <div>
-          <p>Exercise Time: {total} Minutes</p>
-        </div>
-    );
+
+  return (
+    <div>
+      <p>Exercise Time: {total} Minutes</p>
+    </div>
+  );
 };
 
 export default Time;
